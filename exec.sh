32d12de8156a577fd41b8d872482b52f95bd9782
shelydiviy <telephon.inet@gmail.com>
2025-02-04 20:14:52 +0300

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -1,5 +1,5 @@
 iptables -A PREROUTING -t nat -i eth0 -p udp --dport $1 -j REDIRECT --to-port $2
 cd csserv/csserv
-screen -AmdS fake_sv ./hlds_run -game cstrike -ip 192.168.1.249 -port $1 +map de_dust2 +sys_tickrate 1 -sys_ticrate 1 -pingboost 3
+screen -AmdS fake_sv ./hlds_run -game cstrike -ip 192.168.1.249 -port $1 +map de_dust2 +sys_tickrate 1 -sys_ticrate 1 -pingboost 1
 cd ../..
 screen -AmdS red ./a.out $2
